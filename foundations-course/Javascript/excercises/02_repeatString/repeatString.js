d217f774b76a478338234040d76236f4f7064094
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-06 17:24:19 -0400

    Completed exercise 2

--- foundations-course/Javascript/excercises/02_repeatString/repeatString.js
+++ foundations-course/Javascript/excercises/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (str, count) {
+  if (count < 0) return "ERROR";
+  let concatedStr = "";
+  while (count) {
+    concatedStr += str;
+    count--;
+  }
 
+  return concatedStr;
 };
 
 // Do not edit below this line
