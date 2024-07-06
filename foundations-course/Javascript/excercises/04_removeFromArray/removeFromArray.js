fa9cf5e6f4cc68e38d473a6cab585eb7c76f5b04
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-06 18:23:04 -0400

    Completed exercises 4

--- foundations-course/Javascript/excercises/04_removeFromArray/removeFromArray.js
+++ foundations-course/Javascript/excercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (arr, ...remove) =>
+  arr.filter((a) => !remove.includes(a));
 
 // Do not edit below this line
 module.exports = removeFromArray;
