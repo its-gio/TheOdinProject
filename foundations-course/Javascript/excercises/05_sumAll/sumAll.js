c54bdc4fea1dbf727ed90256679cfa90acbfb444
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-06 21:56:01 -0400

    Completed exercises 5

--- foundations-course/Javascript/excercises/05_sumAll/sumAll.js
+++ foundations-course/Javascript/excercises/05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (firstNum, secondNum) {
+  if (
+    firstNum < 0 ||
+    secondNum < 0 ||
+    !Number.isInteger(firstNum) ||
+    !Number.isInteger(secondNum)
+  )
+    return "ERROR";
+  let [min, max] = [firstNum, secondNum].sort((a, b) => a - b);
+  let summedNumbers = 0;
+  for (let i = min; i <= max; i++) {
+    summedNumbers += i;
+  }
+  return summedNumbers;
 };
 
 // Do not edit below this line
