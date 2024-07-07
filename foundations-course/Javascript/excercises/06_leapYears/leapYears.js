6f1b4d1381ed6ddfcf7a2ba1d7a632fb1bb1fd7d
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-06 22:16:30 -0400

    Finished solution for exercise 6

--- foundations-course/Javascript/excercises/06_leapYears/leapYears.js
+++ foundations-course/Javascript/excercises/06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // %4 !%100 %400
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
