85732fd391d0c65480c95a8c788986107671be4e
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-10 19:16:31 -0400

    Adding solved calculator exercises

--- foundations-course/Javascript/excercises/08_calculator/calculator.js
+++ foundations-course/Javascript/excercises/08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((acc, curr) => {
+    return (acc += curr);
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((acc, curr) => {
+    if (!acc) return curr;
+    return acc * curr;
+  }, 0);
 };
 
-const power = function() {
-	
+const power = function (base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) return 1;
+  for (let i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
 };
 
 // Do not edit below this line
@@ -29,5 +38,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
