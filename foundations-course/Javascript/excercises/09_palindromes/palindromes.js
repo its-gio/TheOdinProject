553f31f62142d91b3b8034c42da8fa4120a12ee4
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-10 19:33:00 -0400

    Adding solved exercises 9

--- foundations-course/Javascript/excercises/09_palindromes/palindromes.js
+++ foundations-course/Javascript/excercises/09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = (word) => {
+  let strippedWord = word.toLowerCase().replace(/\W/g, "");
+  return strippedWord === strippedWord.split("").reverse().join("");
 };
 
 // Do not edit below this line
