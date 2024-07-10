6a6da210f147b3bc0c4cea4912851440491a8517
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-10 18:11:37 -0400

    Added grid size functionality

--- foundations-course/Etch-A-Sketch/scripts.js
+++ foundations-course/Etch-A-Sketch/scripts.js
@@ -1,18 +1,32 @@
 const layout = document.querySelector("#layout");
+const numberInput = document.querySelector("#grid-size");
+const newGridBtn = document.querySelector("#new-grid");
 
 // Custimize grid size?
 let gridSize = 16;
 // Custimize grid color?
 // let backgroundColor = "orange";
-
-for (let i = 0; i < gridSize * gridSize; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.classList.add("gridSquare");
-  gridSquare.onmouseover = colorSquare;
-  layout.appendChild(gridSquare);
+function CreateGrid() {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.classList.add("gridSquare");
+    gridSquare.setAttribute("style", `width: calc(100% / ${gridSize})`);
+    gridSquare.onmouseover = colorSquare;
+    layout.appendChild(gridSquare);
+  }
 }
 
 function colorSquare(e) {
-  console.log("Works?");
   return e.target.classList.toggle("colored");
 }
+
+newGridBtn.addEventListener("click", (e) => {
+  const oldGrid = document.querySelectorAll(".gridSquare");
+
+  oldGrid.forEach((oldSquare) => layout.removeChild(oldSquare));
+  gridSize = Math.floor(numberInput.value);
+  console.log(gridSize);
+  CreateGrid();
+});
+
+CreateGrid();
