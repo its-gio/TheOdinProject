baba65550a49b4176f416dbc54f56aa223277552
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-11 15:54:06 -0400

    Completed exercises 12

--- foundations-course/Javascript/excercises/12_findTheOldest/findTheOldest.js
+++ foundations-course/Javascript/excercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((acc, curr) => {
+    let yearOfDeath;
+    curr.yearOfDeath
+      ? (yearOfDeath = curr.yearOfDeath)
+      : (yearOfDeath = new Date().getFullYear());
 
+    if (JSON.stringify(acc) === "{}")
+      return { age: yearOfDeath - curr.yearOfBirth, ...curr };
+
+    return yearOfDeath - curr.yearOfBirth > acc.age ? curr : acc;
+  }, {});
 };
 
 // Do not edit below this line
