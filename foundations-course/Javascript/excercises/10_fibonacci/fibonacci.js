7934260bc225de874ea0abc6ff1e26c63e20df21
its-gio <9650600+its-gio@users.noreply.github.com>
2024-07-11 14:45:08 -0400

    Completed exercises 10

--- foundations-course/Javascript/excercises/10_fibonacci/fibonacci.js
+++ foundations-course/Javascript/excercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let count;
+  count = parseFloat(num);
+  if (count === 0) return 0;
+  if (count < 0) return "OOPS";
 
+  if (count <= 2) return 1;
+  const fib = [0, 1];
+  for (let i = 2; i <= count; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[count];
 };
 
 // Do not edit below this line
